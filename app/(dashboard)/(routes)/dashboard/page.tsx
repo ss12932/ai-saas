67112c62c343f646925deffab450d5aa4773df20
ss12932 <ss12932@my.bristol.ac.uk>
2023-07-25 08:42:33 +0100

    initial sidebar done

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,9 @@
 import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
 
 export default function DashboardPage() {
   return (
     <div>
       <p>Dashboard Page (protected)</p>
-      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
---
+++ components/sidebar.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+const Sidebar = () => {
+  return (
+    <div className="space-y-4 py-4 flex flex-col h-full bg-[#11827] text-white"></div>
+  );
+};
+
+export default Sidebar;
