ebf6ca864c472ed1d7666d3d7d1b0c4b9ef120d9
ss12932 <ss12932@my.bristol.ac.uk>
2023-08-01 20:46:47 +0100

    completed stripe monthly subscription

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -23,6 +23,8 @@ import {
 import { Card } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+import axios from "axios";
+import { useState } from "react";
 
 const tools = [
   {
@@ -59,6 +61,20 @@ const tools = [
 
 export const ProModal = () => {
   const proModal = useProModal();
+  const [loading, setLoading] = useState(false);
+
+  const onSubscribe = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get("/api/stripe");
+
+      window.location.href = (await response).data.url;
+    } catch (err: any) {
+      console.log("[STRIPE_CLIENT_ERROR]: ", err);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
       <DialogContent>
@@ -89,7 +105,12 @@ export const ProModal = () => {
           </DialogDescription>
         </DialogHeader>
         <DialogFooter>
-          <Button size="lg" variant="premium" className="w-full">
+          <Button
+            onClick={onSubscribe}
+            size="lg"
+            variant="premium"
+            className="w-full"
+          >
             Upgrade
             <Zap className="w-4 h-4 ml-2 fill-white" />
           </Button>
